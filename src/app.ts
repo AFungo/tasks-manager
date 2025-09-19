499959665b09f061f3071508bd4f866905a32b4f
AFungo <augustofungo@gmail.com>
2025-09-19 18:26:05 -0300

    Update app

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
-
+import tasksRouter from './routes/task.routes';
 
 
 const app = express();
@@ -12,5 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.get('/', (req, res) => res.json({ ok:true, message: 'API running'}));
+
+app.use('/api/tasks', tasksRouter);
 
 export default app;
